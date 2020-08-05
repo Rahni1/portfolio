6c5dd1ce4627399f57f727d3118a14a2502c98f5
RJDdev <rahnidemeis@gmail.com>
2020-08-05 18:09:28 +0200

    Swap buttons & rename images

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-$(document).ready(function () {
-  let $btns = $(".project-area .button-group button");
+// $(document).ready(function () {
+//   let $btns = $(".project-area .button-group button");
 
-  $btns.click(function (e) {
-    $(".project-area .button-group button").removeClass("active");
-    e.target.classList.add("active");
+//   $btns.click(function (e) {
+//     $(".project-area .button-group button").removeClass("active");
+//     e.target.classList.add("active");
 
-    let selector = $(e.target).attr("data-filter");
-    $(".project-area .grid").isotope({
-      filter: selector,
-    });
+//     let selector = $(e.target).attr("data-filter");
+//     $(".project-area .grid").isotope({
+//       filter: selector,
+//     });
 
-    return false;
-  });
-});
+//     return false;
+//   });
+// });
 
 // STICKY NAVBAR
 $(document).ready(function () {
